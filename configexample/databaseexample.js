5eec19fbe8e1c8e4e4dc7859852d8cad0d9b808b
Graeme MacDougall <gmacdoug@gmail.com>
2018-01-25 15:16:19 -0600

    add config examples

---
+++ configexample/databaseexample.js
@@ -0,0 +1,8 @@
+//all example files are made to be uploaded to GitHub as an example for the file
+//comments are added to show what data is required or missing
+//this is done for security reasons and to protect database passwords, usernames, and secrets
+
+module.exports = {
+	database: //path to database
+	secret: //insert a secret here
+}
