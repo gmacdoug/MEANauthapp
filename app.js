4e9502ef3be0b14c077de73aed16d4904c159bab
Graeme MacDougall <gmacdoug@gmail.com>
2018-01-24 19:21:55 -0600

    fix typo in app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express();
 
 const port = 3000;
 
-app.listen(port,()=> {
+app.get('/', (req, res) => {
+	res.send("Invalid Endpoint");
+});
+
+app.listen(port,() => {
 	console.log('Server started on port ' +port);
 });
